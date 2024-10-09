e5d07bca6861edff56247619d004c77d03a80a51
begie1 <jbergeline1@gmail.com>
2024-10-09 19:42:05 +0200

    quete2

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt=""
+      />
+      <figcaption>
+        <h1> Bulbasaur</h1>
+      </figcaption>
+    </figure>
+  );
+}
+export default PokemonCard;
